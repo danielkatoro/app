74123f3557acf2958b99aa7a3f3dce6ce1d35dc3
danielkatoro <danielkatoro@gmail.com>
2021-03-07 20:15:55 +0100

    CURRENT_TIMESTAMP

--- database/migrations/2021_03_05_144510_add_published_at_to_pub_table.php
+++ database/migrations/2021_03_05_144510_add_published_at_to_pub_table.php
@@ -16,7 +16,7 @@ class AddPublishedAtToPubTable extends Migration
         Schema::table('pub', function (Blueprint $table) {
             $table->dateTime('published_at')->after('published')
                                             ->nullable()
-                                            ->default(now());
+                                            ->default(DB::raw('CURRENT_TIMESTAMP');
         });
     }
 
